e72b416bad0c09628cde1b072e25a8806dd540d0
Carson Tam <carson.tam@ucsf.edu>
2015-10-15 10:38:14 -0700

    Removed random execution time that limits to run only 20% of the time.

--- lib.php
+++ lib.php
@@ -29,99 +29,90 @@
 function local_archiveup_cron() {
     global $DB, $CFG;
 
-    // Run cleanup core cron jobs, but not every time since they aren't too important.
-    // These don't have a timer to reduce load, so we'll use a random number
-    // to randomly choose the percentage of times we should run these jobs.
-    srand ((double) microtime() * 10000000);
-    $random100 = rand(0,100);
-    if ($random100 < 20) {     // Approximately 20% of the time.
-        mtrace(" ArchiveUp: Running archive tasks...");
+    $dbman = $DB->get_manager();
+    $timenow  = time();
+    mtrace(" ArchiveUp: Running archive tasks (current time: ".date('r',$timenow)."...");
 
-        $dbman = $DB->get_manager();
-        $timenow  = time();
-        mtrace(" ArchiveUp current time: ".date('r',$timenow));
+    // c.f. Delete old logs in lib/cronlib.php
+    $loglifetime = get_config('local_archiveup', 'loglifetime');
+    if (!empty($loglifetime)) {  // value in days
 
-        // c.f. Delete old logs in lib/cronlib.php
-        $loglifetime = get_config('local_archiveup', 'loglifetime');
-        if (!empty($loglifetime)) {  // value in days
+        $loglifetime = $timenow - ($loglifetime * 3600 * 24);
+        $lifetimep = array($loglifetime);
 
-            $loglifetime = $timenow - ($loglifetime * 3600 * 24);
-            $lifetimep = array($loglifetime);
+        $archivetablename = "log_archiveup";
+        if ($dbman->table_exists($archivetablename)) {
+            mtrace("  Looking for old record(s) in legacy log table...");
 
-            $archivetablename = "log_archiveup";
-            if ($dbman->table_exists($archivetablename)) {
-                mtrace("  Looking for old record(s) in legacy log table...");
+            $start = time();
+            $totalrecords = 0;
+            while ($min = $DB->get_field_select("log", "MIN(time)", "time < ?", $lifetimep)) {
+                // Break this down into chunks to avoid transaction for too long and generally thrashing database.
+                // Experiments suggest deleting one day takes up to a few seconds; probably a reasonable chunk size usually.
+                // If the cleanup has just been enabled, it might take e.g a month to clean the years of logs.
+                $params = array(min($min + 3600 * 24, $loglifetime));
+                $totalrecords += $DB->count_records_select("log", "time < ?", $params);
 
-                $start = time();
-                $totalrecords = 0;
-                while ($min = $DB->get_field_select("log", "MIN(time)", "time < ?", $lifetimep)) {
-                    // Break this down into chunks to avoid transaction for too long and generally thrashing database.
-                    // Experiments suggest deleting one day takes up to a few seconds; probably a reasonable chunk size usually.
-                    // If the cleanup has just been enabled, it might take e.g a month to clean the years of logs.
-                    $params = array(min($min + 3600 * 24, $loglifetime));
-                    $totalrecords += $DB->count_records_select("log", "time < ?", $params);
-
-                    $transaction = $DB->start_delegated_transaction();
-                    $DB->execute("INSERT INTO {{$archivetablename}} SELECT * FROM {log} WHERE time < ?", $params);
-                    $DB->delete_records_select("log", "time < ?", $params);
-                    $transaction->allow_commit();
-                    if (time() > $start + 300) {
-                        // Do not churn on log deletion for too long each run.
-                        break;
-                    }
+                $transaction = $DB->start_delegated_transaction();
+                $DB->execute("INSERT INTO {{$archivetablename}} SELECT * FROM {log} WHERE time < ?", $params);
+                $DB->delete_records_select("log", "time < ?", $params);
+                $transaction->allow_commit();
+                if (time() > $start + 300) {
+                    // Do not churn on log deletion for too long each run.
+                    break;
                 }
-                mtrace("  archived $totalrecords old record(s) from legacy log table.");
             }
-
-            $archivetablename = "logstore_standard_log_au";
-            if ($dbman->table_exists($archivetablename)) {
-                mtrace("  Looking for old record(s) in standard logstore...");
-
-                $start = time();
-                $totalrecords = 0;
-                while ($min = $DB->get_field_select("logstore_standard_log", "MIN(timecreated)", "timecreated < ?", $lifetimep)) {
-                    // Break this down into chunks to avoid transaction for too long and generally thrashing database.
-                    // Experiments suggest deleting one day takes up to a few seconds; probably a reasonable chunk size usually.
-                    // If the cleanup has just been enabled, it might take e.g a month to clean the years of logs.
-                    $params = array(min($min + 3600 * 24, $loglifetime));
-                    $totalrecords += $DB->count_records_select("logstore_standard_log", "timecreated < ?", $params);
-
-                    $transaction = $DB->start_delegated_transaction();
-                    $DB->execute("INSERT INTO {{$archivetablename}} SELECT * FROM {logstore_standard_log} WHERE timecreated < ?", $params);
-                    $DB->delete_records_select("logstore_standard_log", "timecreated < ?", $params);
-                    $transaction->allow_commit();
-                    if (time() > $start + 300) {
-                        // Do not churn on log deletion for too long each run.
-                        break;
-                    }
-                }
-                mtrace("  archived $totalrecords old record(s) from standard logstore.");
-            }
+            mtrace("  ... archived $totalrecords old record(s) from legacy log table.");
         }
 
-        // c.f. grade_cron() in lib/gradelib.php
-        $gradehistorylifetime = get_config('local_archiveup', 'gradehistorylifetime');
-        if (!empty($gradehistorylifetime)) {
-            $histlifetime = $timenow - ($gradehistorylifetime * 3600 * 24);
-            $tables = array('grade_outcomes_history', 'grade_categories_history', 'grade_items_history', 'grade_grades_history', 'scale_history');
-            foreach ($tables as $table) {
-                $archivetable = $table . '_au';
-                if ($dbman->table_exists($archivetable)) {
-                    mtrace("  Looking for old grade history records in '$table'...");
-                    $totalrecords = $DB->count_records_select($table, "timemodified < ?", array($histlifetime));
-                    if ($totalrecords > 0) {
-                        $transaction = $DB->start_delegated_transaction();
-                        $DB->execute("INSERT INTO {{$archivetable}} SELECT * FROM {{$table}} WHERE timemodified < ?", array($histlifetime));
-                        $DB->delete_records_select($table, "timemodified < ?", array($histlifetime));
-                        $transaction->allow_commit();
-                        mtrace("  Moved $totalrecords old grade history record(s) in '$table' to '$archivetable'.");
-                    } else {
-                        mtrace("  nothing to archive.");
-                    }
+        $archivetablename = "logstore_standard_log_au";
+        if ($dbman->table_exists($archivetablename)) {
+            mtrace("  Looking for old record(s) in standard logstore...");
+
+            $start = time();
+            $totalrecords = 0;
+            while ($min = $DB->get_field_select("logstore_standard_log", "MIN(timecreated)", "timecreated < ?", $lifetimep)) {
+                // Break this down into chunks to avoid transaction for too long and generally thrashing database.
+                // Experiments suggest deleting one day takes up to a few seconds; probably a reasonable chunk size usually.
+                // If the cleanup has just been enabled, it might take e.g a month to clean the years of logs.
+                $params = array(min($min + 3600 * 24, $loglifetime));
+                $totalrecords += $DB->count_records_select("logstore_standard_log", "timecreated < ?", $params);
+
+                $transaction = $DB->start_delegated_transaction();
+                $DB->execute("INSERT INTO {{$archivetablename}} SELECT * FROM {logstore_standard_log} WHERE timecreated < ?", $params);
+                $DB->delete_records_select("logstore_standard_log", "timecreated < ?", $params);
+                $transaction->allow_commit();
+                if (time() > $start + 300) {
+                    // Do not churn on log deletion for too long each run.
+                    break;
                 }
             }
+            mtrace("  ... archived $totalrecords old record(s) from standard logstore.");
         }
-
-        mtrace(" Archived old log records.");
     }
+
+    // c.f. grade_cron() in lib/gradelib.php
+    $gradehistorylifetime = get_config('local_archiveup', 'gradehistorylifetime');
+    if (!empty($gradehistorylifetime)) {
+        $histlifetime = $timenow - ($gradehistorylifetime * 3600 * 24);
+        $tables = array('grade_outcomes_history', 'grade_categories_history', 'grade_items_history', 'grade_grades_history', 'scale_history');
+        foreach ($tables as $table) {
+            $archivetable = $table . '_au';
+            if ($dbman->table_exists($archivetable)) {
+                mtrace("  Looking for old grade history records in '$table'...");
+                $totalrecords = $DB->count_records_select($table, "timemodified < ?", array($histlifetime));
+                if ($totalrecords > 0) {
+                    $transaction = $DB->start_delegated_transaction();
+                    $DB->execute("INSERT INTO {{$archivetable}} SELECT * FROM {{$table}} WHERE timemodified < ?", array($histlifetime));
+                    $DB->delete_records_select($table, "timemodified < ?", array($histlifetime));
+                    $transaction->allow_commit();
+                    mtrace("  ... moved $totalrecords old grade history record(s) in '$table' to '$archivetable'.");
+                } else {
+                    mtrace("  ... nothing to archive.");
+                }
+            }
+        }
+    }
+
+    mtrace(" ... archived old log records.");
 }
